7bbd6aba43054e135fb7a4d4b2e40a0430271d80
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2023-10-30 14:59:50 +0200

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,6 +2,4 @@
 
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-
-# Print "object-oriented programming with Python" followed by a new line
 print(str.split(",")[2].split()[4][:-1])
