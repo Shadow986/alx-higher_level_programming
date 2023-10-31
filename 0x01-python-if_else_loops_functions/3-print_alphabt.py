dc05440d361e99cccdd491ffd92d01e405b82041
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2023-10-31 12:25:21 +0200

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for character in range(97, 123):
-    if chr(character) not in ['q', 'e']:
+    if (chr(character) not in ['q', 'e']):
         print("{:c}".format(character), end='')
 print()
