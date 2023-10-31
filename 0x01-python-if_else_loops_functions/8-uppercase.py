11bae0b8a585fffb139e4a4019b7bb882e68ccf8
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2023-10-31 13:24:46 +0200

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,6 @@ def uppercase(string):
     for char in string:
         uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
         print(uppercase_char, end="")
-    print()
+    ::w
+def uppercase(string):
+    string_new = ""
