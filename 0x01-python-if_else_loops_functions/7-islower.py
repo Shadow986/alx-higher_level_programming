a002f7cc68e5282329ac707ccbe41c95ab5d4902
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2023-10-31 13:12:29 +0200

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, ones_digit), end="")
-        if tens_digit != 8 or ones_digit != 9:
-            print(", ", end="")
-        else:
-            print()
+def islower(c):
+    if ord(c) > 96:
+        return True
+    else:
+        return False
