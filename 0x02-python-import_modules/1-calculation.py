7783fcdef9e345e3551c6faa7135e9ffd24b4c3b
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2023-11-02 08:03:44 +0200

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-
-a = 10
-b = 5
-
-from calculator_1 import add, subtract, multiply, divide
-
-add_result = add(a, b)
-subtract_result = subtract(a, b)
-multiply_result = multiply(a, b)
-divide_result = divide(a, b)
-
-print("Addition: {}".format(add_result))
-print("Subtraction: {}".format(subtract_result))
-print("Multiplication: {}".format(multiply_result))
-print("Division: {}".format(divide_result))
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    funcs = [('+', add), ('-', sub), ('*', mul), ('/', div)]
+    for func in funcs:
+        print('{} {} {} = {}'.format(a, func[0], b, func[1](a, b)))
