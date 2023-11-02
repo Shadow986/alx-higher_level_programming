a24e1e30dd7c232414b049eb8dc8fdf5fcf91738
Shadow986 <archie986jr@gmail.com>
2023-11-01 23:10:43 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+
+arguments = sys.argv[1:]
+num_arguments = len(arguments)
+
+print("Number of argument{}: {}".format("s" if num_arguments != 1 else "", num_arguments))
+print(":." if num_arguments == 0 else "")
+
+for i, argument in enumerate(arguments, start=1):
+    print("{}: {}".format(i, argument))
