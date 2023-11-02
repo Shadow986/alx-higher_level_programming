76eb4706e425f30b546fc6bab35c3275cdb98578
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2023-11-02 12:53:03 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+
 import sys
 
 arguments = sys.argv[1:]
 num_arguments = len(arguments)
 
-print("Number of argument{}: {}".format("s" if num_arguments != 1 else "", num_arguments))
-print(":." if num_arguments == 0 else "")
+print('{} argument{}{}'.format(
+    num_arguments, 's' * (num_arguments != 1),
+    ':' if num_arguments > 0 else '.'
+))
 
 for i, argument in enumerate(arguments, start=1):
-    print("{}: {}".format(i, argument))
+    print('{}: {}'.format(i, argument))
