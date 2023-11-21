f7f7511d98b4ee9fd2b6140c46d26a29e5869e62
Shadow986 <archie986jr@gmail.com>
2023-11-20 22:30:10 -0800

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is the "my_module" module.
+
+The my_module module defines the Square class.
+"""
+
+
+class Square:
+    """
+    This is the Square class.
+
+    The Square class defines an empty class named Square.
+    """
+
+    def __init__(self):
+        """
+        This is the constructor method for the Square class.
+
+        The constructor defines an empty method.
+        """
+        pass
