3689a7e7e9b1e91b8bd54cc6d3289ae89b013f11
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2023-11-27 13:33:13 +0200

    Update 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
         rectangle with the character(s) stored in print_symbol."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join([str(self.print_symbol) * self.__width
+        return "\n".join([str(self.print_symbol) * self.__width,
             for _ in range(self.__height)])
 
     def __repr__(self):
