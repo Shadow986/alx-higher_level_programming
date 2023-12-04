0fdac03b78247b23ee1197a1fde2dda3bd513e8e
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2023-12-04 08:32:29 +0200

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -3,6 +3,7 @@
 This module contains a class BaseGeometry with a public instance method.
 """
 
+
 class BaseGeometry:
     """
     A class BaseGeometry with a public instance method.
@@ -10,6 +11,7 @@ class BaseGeometry:
 
     def area(self):
         """
-        Public instance method that raises an Exception with the message area() is not implemented.
+        Public instance method that raises an Exception
+        with the message area() is not implemented.
         """
         raise Exception("area() is not implemented")
