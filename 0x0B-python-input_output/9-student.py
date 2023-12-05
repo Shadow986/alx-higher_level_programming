f9c888032c80ccebcb9cf78676d567f207ab629a
Shadow986 <archie986jr@gmail.com>
2023-12-05 03:46:43 -0800

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module contains a class Student that defines a student by
+public instance attributes and a public method
+"""
+
+
+class Student:
+    """
+    A class Student that defines a student by
+    public instance attributes and a public method
+
+    Attributes:
+        first_name (str): the first name of the student
+        last_name (str): the last name of the student
+        age (int): the age of the student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with first_name, last_name and age
+
+        Args:
+            first_name (str): the first name of the student
+            last_name (str): the last name of the student
+            age (int): the age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance
+
+        Returns:
+            dict: a dictionary containing all attributes of the Student instance
+        """
+        return self.__dict__
